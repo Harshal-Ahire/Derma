c4acdc70cf98836b2aea131fd4233ad43c143b1a
Harshal A. <141637336+Harshal-Ahire@users.noreply.github.com>
2025-10-02 13:33:33 +0530

    Update UploadComponent.js

--- frontend/src/components/UploadComponent.js
+++ frontend/src/components/UploadComponent.js
@@ -3,12 +3,10 @@ import React, { useRef } from "react";
 export default function UploadComponent({ selectedFile, setSelectedFile }) {
   const fileInputRef = useRef(null);
 
-  // Triggered when user selects a file
   const handleFileChange = (e) => {
     setSelectedFile(e.target.files[0]);
   };
 
-  // Trigger file input when user clicks the upload box
   const handleUploadClick = () => {
     fileInputRef.current.click();
   };
@@ -25,23 +23,25 @@ export default function UploadComponent({ selectedFile, setSelectedFile }) {
         style={{ display: "none" }}
       />
 
-      <div
-        role="button"
-        tabIndex="0"
-        className="w-full border border-blueBorder rounded px-3 py-1 flex items-center justify-center bg-white hover:bg-blue-50 cursor-pointer"
-        onClick={handleUploadClick}
-      >
-        <svg width="15px" height="15px" viewBox="0 0 15 15" className="mr-2">
-          <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-            <g fill="#3B82F6">
-              <rect x="6" y="1" width="3" height="13"></rect>
-              <rect x="1" y="6" width="13" height="3"></rect>
+      <div className="flex items-center justify-center w-full">
+        <div
+          role="button"
+          tabIndex="0"
+          className="border border-blueBorder rounded px-3 py-1 flex items-center justify-center bg-white hover:bg-blue-50 cursor-pointer"
+          onClick={handleUploadClick}
+        >
+          <svg width="15px" height="15px" viewBox="0 0 15 15" className="mr-2">
+            <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+              <g fill="#3B82F6">
+                <rect x="6" y="1" width="3" height="13"></rect>
+                <rect x="1" y="6" width="13" height="3"></rect>
+              </g>
             </g>
-          </g>
-        </svg>
-        <span className="text-blueBorder text-xs">
-          {selectedFile ? selectedFile.name : "Skin Image"}
-        </span>
+          </svg>
+          <span className="text-blueBorder text-xs">
+            {selectedFile ? selectedFile.name : "Skin Image"}
+          </span>
+        </div>
       </div>
 
       <p className="text-xs text-gray-400 mt-2">JPG/PNG</p>
